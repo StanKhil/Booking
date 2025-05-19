3bc2528af657be2efc433f6b65378b7bd9e4a98b
Stanislav <stankhil@gmail.com>
2025-05-19 17:37:07 +0300

    added test for feedback and booking models

---
+++ UnitTest/Models/BookingModelTest.cs
@@ -0,0 +1,116 @@
+﻿using Booking.Data.Entities;
+using Booking.Data;
+using Booking.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace UnitTest.Models
+{
+    [TestClass]
+    public class BookingModelTest
+    {
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var context = new DataContext();
+            var model = new BookingModel(context);
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        public async Task CreateBookingAsync()
+        {
+            var context = new DataContext();
+            var userId = Guid.NewGuid();
+            var realtyId = Guid.NewGuid();
+
+            context.UserAccesses.Add(new UserAccess { Id = userId });
+            context.Realties.Add(new Realty
+            {
+                Id = realtyId,
+                BookingItems = new List<BookingItem>(),
+                DeletedAt = null
+            });
+            await context.SaveChangesAsync();
+
+            var model = new BookingModel(context);
+            var start = DateTime.Now.AddDays(1);
+            var end = DateTime.Now.AddDays(3);
+
+            var result = await model.CreateBookingAsync(userId, realtyId, start, end);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task CreateBookingAsyncOverlappingDates()
+        {
+            var context = new DataContext();
+            var userId = Guid.NewGuid();
+            var realtyId = Guid.NewGuid();
+
+            var existingBooking = new BookingItem
+            {
+                Id = Guid.NewGuid(),
+                StartDate = DateTime.Today.AddDays(2),
+                EndDate = DateTime.Today.AddDays(5),
+                DeletedAt = null
+            };
+
+            var realty = new Realty
+            {
+                Id = realtyId,
+                BookingItems = new List<BookingItem> { existingBooking },
+                DeletedAt = null
+            };
+
+            context.UserAccesses.Add(new UserAccess { Id = userId });
+            context.Realties.Add(realty);
+            await context.SaveChangesAsync();
+
+            var model = new BookingModel(context);
+
+            var overlapStart = DateTime.Today.AddDays(3);
+            var overlapEnd = DateTime.Today.AddDays(6);
+
+            var result = await model.CreateBookingAsync(userId, realtyId, overlapStart, overlapEnd);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task DeleteBookingAsync()
+        {
+            var context = new DataContext();
+            var bookingId = Guid.NewGuid();
+            var realtyId = Guid.NewGuid();
+
+            var booking = new BookingItem
+            {
+                Id = bookingId,
+                RealtyId = realtyId,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(1),
+                DeletedAt = null
+            };
+
+            var realty = new Realty
+            {
+                Id = realtyId,
+                BookingItems = new List<BookingItem> { booking },
+                DeletedAt = null
+            };
+
+            context.BookingItems.Add(booking);
+            context.Realties.Add(realty);
+            await context.SaveChangesAsync();
+
+            var model = new BookingModel(context);
+            var result = await model.DeleteBookingAsync(bookingId);
+
+            Assert.IsTrue(result);
+            Assert.IsNotNull(context.BookingItems.First().DeletedAt);
+        }
+    }
+}
---
+++ UnitTest/Models/FeedbackModelTest.cs
@@ -0,0 +1,106 @@
+﻿using Booking.Data.Entities;
+using Booking.Data;
+using Booking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Models
+{
+    [TestClass]
+    public class FeedbackModelTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var model = new FeedbackModel();
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        public async Task CreateFeedbackAsync()
+        {
+            var context = new DataContext();
+
+            var userId = Guid.NewGuid();
+            var realtyId = Guid.NewGuid();
+
+            context.UserAccesses.Add(new UserAccess { Id = userId });
+            context.Realties.Add(new Realty
+            {
+                Id = realtyId,
+                Feedbacks = new List<Feedback>(),
+                DeletedAt = null
+            });
+            await context.SaveChangesAsync();
+
+            var model = new FeedbackModel();
+            model.GetType().GetField("dataContext", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                ?.SetValue(model, context);
+
+            var result = await model.CreateFeedbackAsync(userId, realtyId, "Nice place", 5);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task CreateFeedbackAsyncEmptyMessage()
+        {
+            var model = new FeedbackModel();
+            var result = await model.CreateFeedbackAsync(Guid.NewGuid(), Guid.NewGuid(), "", 4);
+            Assert.IsFalse(result);
+        }
+
+
+
+        [TestMethod]
+        public async Task DeleteFeedbackAsync()
+        {
+            var context = new DataContext();
+            var feedbackId = Guid.NewGuid();
+            var realtyId = Guid.NewGuid();
+
+            var feedback = new Feedback
+            {
+                Id = feedbackId,
+                RealtyId = realtyId,
+                DeletedAt = null
+            };
+
+            var realty = new Realty
+            {
+                Id = realtyId,
+                DeletedAt = null,
+                Feedbacks = new List<Feedback> { feedback }
+            };
+
+            context.Feedbacks.Add(feedback);
+            context.Realties.Add(realty);
+            await context.SaveChangesAsync();
+
+            var model = new FeedbackModel();
+            model.GetType().GetField("dataContext", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                ?.SetValue(model, context);
+
+            var result = await model.DeleteFeedbackAsync(feedbackId);
+            Assert.IsTrue(result);
+
+            var updated = await context.Feedbacks.FirstOrDefaultAsync(f => f.Id == feedbackId);
+            Assert.IsNotNull(updated?.DeletedAt);
+        }
+
+        [TestMethod]
+        public async Task DeleteFeedbackAsyncNotFound()
+        {
+            var context = new DataContext();
+            var model = new FeedbackModel();
+            model.GetType().GetField("dataContext", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                ?.SetValue(model, context);
+
+            var result = await model.DeleteFeedbackAsync(Guid.NewGuid());
+            Assert.IsFalse(result);
+        }
+    }
+}
